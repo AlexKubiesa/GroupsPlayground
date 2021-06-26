f1218d8d4713a9d2b39132436badc0bde8caaf5a
Alex Kubiesa <alex.kubiesa@outlook.com>
2021-06-26 02:05:37 +0100

    Start implementing Blazor app.

---
+++ src/GroupsPlayground.Blazor/Models/GroupPropertiesModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupsPlayground.Blazor.Models
+{
+    public class GroupPropertiesModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 6, ErrorMessage = "Size must be between 1 and 6.")]
+        public int Size { get; set; }
+    }
+}
