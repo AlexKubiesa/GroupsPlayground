2dd093109834d491c4e0c304d8c4fe012dc7bcc5
Alex Kubiesa <alex.kubiesa@outlook.com>
2021-07-06 18:48:41 +0100

    Factor out GroupOperation page.

---
+++ src/GroupsPlayground.Blazor/Components/CreateGroup/GroupOperation.razor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using GroupsPlayground.Domain;
+
+namespace GroupsPlayground.Blazor.Components.CreateGroup
+{
+    public class GroupOperationModel
+    {
+        private readonly List<GroupOperationElementModel> elements = new();
+
+        public int ElementCount
+        {
+            get => Elements.Count;
+            set
+            {
+                if (value == Elements.Count)
+                    return;
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value));
+
+                CayleyTable = new CayleyTable(Guid.NewGuid(), value);
+
+                elements.Clear();
+                elements.AddRange(CayleyTable.Symbols.Select(x => new GroupOperationElementModel(x)));
+
+                Products = CayleyTable.Products
+                    .Select((row, rowIndex) =>
+                        row.Select((product, columnIndex) => new GroupOperationProductModel(CayleyTable, rowIndex, columnIndex))
+                            .ToArray())
+                    .ToArray();
+            }
+        }
+
+        public CayleyTable CayleyTable { get; private set; }
+        public IReadOnlyList<GroupOperationElementModel> Elements => elements;
+        public GroupOperationProductModel[][] Products { get; private set; }
+        public string ValidationMessage { get; set; }
+    }
+
+    public class GroupOperationElementModel
+    {
+        public GroupOperationElementModel(Symbol symbol) => Symbol = symbol?.ToString();
+
+        public string Symbol { get; }
+    }
+
+    public class GroupOperationProductModel
+    {
+        private readonly CayleyTable cayleyTable;
+        private readonly int firstIndex;
+        private readonly int secondIndex;
+
+        public GroupOperationProductModel(CayleyTable cayleyTable, int firstIndex, int secondIndex)
+        {
+            this.cayleyTable = cayleyTable ?? throw new ArgumentNullException(nameof(cayleyTable));
+            this.firstIndex = firstIndex;
+            this.secondIndex = secondIndex;
+        }
+
+        public string Symbol
+        {
+            get => cayleyTable.Products[firstIndex][secondIndex]?.ToString();
+            set => cayleyTable.Products[firstIndex][secondIndex] =
+                (value == null)
+                    ? null
+                    : new Symbol(value);
+        }
+    }
+}
