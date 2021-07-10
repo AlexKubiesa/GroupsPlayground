208cb0da79bc32a1f670e7178ed8528cbb2cee75
Alex Kubiesa <alex.kubiesa@outlook.com>
2021-07-10 16:30:16 +0100

    Add a form to input permutations to generate a permutation group.

---
+++ src/GroupsPlayground.Blazor/Components/CreateGroup/PermutationGenerators.razor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroupsPlayground.Domain;
+using GroupsPlayground.Domain.Framework;
+
+namespace GroupsPlayground.Blazor.Components.CreateGroup
+{
+    public class PermutationGeneratorsModel
+    {
+        private readonly List<PermutationGeneratorModel> generators = new();
+
+        public int GeneratorCount
+        {
+            get => generators.Count;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                while (generators.Count < value)
+                    generators.Add(new PermutationGeneratorModel());
+                while (generators.Count > value)
+                    generators.RemoveAt(generators.Count - 1);
+            }
+        }
+
+        public IReadOnlyList<PermutationGeneratorModel> Generators => generators;
+
+        public bool Validate() => generators.Select(x => x.Validate()).ToList().All(x => x);
+    }
+
+    public class PermutationGeneratorModel
+    {
+        public string Expression { get; set; }
+
+        public string ValidationMessage { get; private set; }
+
+        public bool Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/GroupsPlayground.Domain/Permutation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GroupsPlayground.Domain.Framework;
+
+namespace GroupsPlayground.Domain
+{
+    public sealed class Permutation : ValueObject<Permutation>
+    {
+        public static Permutation Parse(string expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool EqualsInternal(Permutation other)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override int GetHashCodeInternal()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
